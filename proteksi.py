e4d21119f39af5e3890f5f8caef73dd8ace18336
Adammo sheriffo crocodilo <adammahmoud1748@gmail.com>
2026-03-02 10:06:08 +0700

    Add emergency stop protocol in proteksi.py

---
+++ proteksi.py
@@ -0,0 +1,7 @@
+import os
+
+# Adam's Emergency Stop
+print("MENGAKTIFKAN PROTOKOL PROTEKSI... 🛡️")
+os.system("pkill -f kiamat.sh") # Matiin script kiamat
+os.system("pkill -f cat")       # Berhentiin pengiriman nuklir.txt
+print("SISTEM AMAN. NUKLIR DINONAKTIFKAN. ✅")
